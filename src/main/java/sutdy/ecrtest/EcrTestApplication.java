88c7c47032463893f6d34d98d8e9d88d35a404d9
mmingoo <aldrn0323@naver.com>
2024-10-11 22:57:08 +0900

    ecr 업로드 테스트

---
+++ src/main/java/sutdy/ecrtest/EcrTestApplication.java
@@ -0,0 +1,13 @@
+package sutdy.ecrtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcrTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcrTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sutdy/ecrtest/EcrTestApplicationTests.java
@@ -0,0 +1,13 @@
+package sutdy.ecrtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcrTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
