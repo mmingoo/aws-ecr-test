db85d0e4f4c50176ad285bc9399389d1e90f309d
mmingoo <aldrn0323@naver.com>
2024-10-11 23:39:46 +0900

    test Controller 업데이트

---
+++ src/main/java/sutdy/ecrtest/TestController.java
@@ -0,0 +1,13 @@
+package sutdy.ecrtest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String testController(){
+        return "Hello";
+    }
+}
